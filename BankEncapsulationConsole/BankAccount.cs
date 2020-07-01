79a01d8e453cfb4752f29490d9b993aa4dc801a3
Brenton Phillips <phillips.brenton@gmail.com>
2020-07-01 10:12:46 -0500

    created user deposit account

---
+++ BankEncapsulationConsole/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace BankEncapsulationConsole
+{
+    public class BankAccount
+    {
+
+        private double balance = 0;
+
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+
+        }
+
+        //Encapsulation
+        public void Deposit(double ammount)
+        {
+            balance = ammount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+
+
+
+
+
+
+        public BankAccount()
+        {
+        }
+    }
+}
---
+++ BankEncapsulationConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulationConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"Thank You! Your new balance is now {account.GetBalance()}");
+
+                
+        }
+    }
+}
